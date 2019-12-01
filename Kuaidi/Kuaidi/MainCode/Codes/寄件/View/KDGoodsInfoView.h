e7976834d74f2a2218166e4aa40ebb8323b7a47f
zeroLu <zero.lu@newtype.io>
2019-12-01 12:28:25 +0800

    123456

--- Kuaidi/Kuaidi/MainCode/Codes/寄件/View/KDGoodsInfoView.h
+++ Kuaidi/Kuaidi/MainCode/Codes/寄件/View/KDGoodsInfoView.h
@@ -14,6 +14,7 @@ NS_ASSUME_NONNULL_BEGIN
 @optional
 -(void)selectCellIndexPath:(NSIndexPath *)indexPath;
 -(void)clickConfirmButton;
+-(void)clickCancellOrder;
 @end
 
 @interface KDGoodsInfoView : UITableView
